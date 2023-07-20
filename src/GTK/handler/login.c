d2b5da9995c2472c74acc36e5c35b7c2421ae827
nghhng <huyhoang2014bk@gmail.com>
2023-07-20 14:59:44 +0700

    Submit Button

--- src/GTK/handler/login.c
+++ src/GTK/handler/login.c
@@ -2,7 +2,7 @@
 
 GtkWidget *window;
 GtkWidget *fixed;
-GtkWidget *registerLabel;
+GtkWidget *loginLabel;
 GtkWidget *usernameLabel;
 GtkWidget *passwordLabel;
 GtkWidget *usernameEntry;
@@ -12,14 +12,14 @@ GtkWidget *backButton;
 GtkBuilder *builder; 
 void on_submitButton_clicked(GtkButton *b);
 
-GtkWidget* createRegisterView(int argc, char *argv[]){
+GtkWidget* createloginView(int argc, char *argv[]){
 
-        builder = gtk_builder_new_from_file("xml/register.glade");
+        builder = gtk_builder_new_from_file("xml/login.glade");
         window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
         g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
         fixed = GTK_WIDGET(gtk_builder_get_object(builder, "fixed"));
-        registerLabel = GTK_WIDGET(gtk_builder_get_object(builder, "registerLabel"));
+        loginLabel = GTK_WIDGET(gtk_builder_get_object(builder, "loginLabel"));
         usernameLabel = GTK_WIDGET(gtk_builder_get_object(builder, "usernameLabel"));
         passwordLabel = GTK_WIDGET(gtk_builder_get_object(builder, "passwordLabel"));
         usernameEntry = GTK_WIDGET(gtk_builder_get_object(builder, "usernameEntry"));
